6b4b38f2dc6e599d12653174d2abf2073a48974a
Trainer <kashappaappannavar@gmail.com>
2025-10-24 23:50:18 +0530

    save

---
+++ src/main/java/com/ksa/NexusAppApplication.java
@@ -0,0 +1,13 @@
+package com.ksa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksa/NexusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
